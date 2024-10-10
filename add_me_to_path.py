219eba6a39ddc4faa95e771ea01683aac7825497
Frank Sorenson <sorenson@redhat.com>
2024-10-10 11:16:04 -0500

    add_me_to_path: add the directory (or directories) to the python path
    Add the directory in which add_me_to_path resides to the python path,
    or add the paths given on the command-line.
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

---
+++ add_me_to_path.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys, os
+
+def try_addpath(path):
+	if os.path.isdir(path):
+		if path not in sys.path:
+			sys.path.append(path)
+
+if __name__ == "__main__":
+	check_paths = []
+
+	args = sys.argv[1:]
+
+	if len(args):
+		for newpath in args:
+			try_addpath(newpath)
+    else:
+		try_addpath(os.path.dirname(os.path.realpath(sys.argv[0])))
+
+# vim: sw=4 ts=4 noexpandtab
