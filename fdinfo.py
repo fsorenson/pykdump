291a2943db451471ef51ff7d977b8bfd3b0f7c68
Frank Sorenson <sorenson@redhat.com>
2026-02-03 16:52:09 -0600

    fdinfo: finish adding inotify and add fanotify
    Add the remaining pieces for inotify and fanotify.
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

--- fdinfo.py
+++ fdinfo.py
@@ -24,12 +24,38 @@ def arg_value(arg):
 	except ValueError:
 		return 0
 
+def file_inode(filp):
+	try:
+		filp = readSU("struct file", filp)
+	except Exception as ex:
+		pass
+		return 0
+	try:
+		return filp.f_inode
+	except Exception as ex:
+		try:
+			dentry = readSU("struct dentry", file_get_dentry(filp))
+			return dentry.d_inode
+		except Exception as ex2:
+			print("error in file_inode({:016x}): {}".format(dentry, ex2))
+			pass
+			return 0
+		pass
+	return 0
+
+
 # TODO: show_mark_fhandle in fs/notify/fdinfo.c
 def show_mark_fhandle(inode):
 	print("fhandle NOT IMPLEMENTED", end='')
 
 def fsnotify_conn_inode(conn):
 	return readSU("struct inode", container_of(conn.obj, "struct inode", "i_fsnotify_marks"))
+
+def fsnotify_conn_mount(conn):
+	return readSU("struct mount", container_of(conn.obj, "struct mount", "mnt_fsnotify_marks"))
+
+def fsnotify_conn_sb(conn):
+	return readSU("struct super_block", container_of(conn.obj, "struct super_block", "s_fsnotify_marks"))
 
 #include/uapi/linux/inotify.h
 #define IN_ACCESS               0x00000001      /* File was accessed */
@@ -59,6 +85,56 @@ INOTIFY_USER_MASK = 0x84000fff
 def inotify_mark_user_mask(fsn_mark):
 	return fsn_mark.mask & INOTIFY_USER_MASK
 
+# FSNOTIFY_MARK_FLAGS from include/linux/fsnotify_backend.h
+FSNOTIFY_MARK_FLAG_IGNORED_SURV_MODIFY	= 0x01
+FSNOTIFY_MARK_FLAG_ALIVE		= 0x02
+FSNOTIFY_MARK_FLAG_ATTACHED		= 0x04
+#include/uapi/linux/fanotify.h
+FAN_MARK_FLAGS_C = '''
+/* flags used for fanotify_modify_mark() */
+#define FAN_MARK_ADD            0x00000001
+#define FAN_MARK_REMOVE         0x00000002
+#define FAN_MARK_DONT_FOLLOW    0x00000004
+#define FAN_MARK_ONLYDIR        0x00000008
+/* FAN_MARK_MOUNT is            0x00000010 */
+#define FAN_MARK_IGNORED_MASK   0x00000020
+#define FAN_MARK_IGNORED_SURV_MODIFY    0x00000040
+#define FAN_MARK_FLUSH          0x00000080
+/* FAN_MARK_FILESYSTEM is       0x00000100 */
+
+/* These are NOT bitwise flags.  Both bits can be used togther.  */
+#define FAN_MARK_INODE          0x00000000
+#define FAN_MARK_MOUNT          0x00000010
+#define FAN_MARK_FILESYSTEM     0x00000100
+
+/* These are NOT bitwise flags.  Both bits can be used togther.  */
+#define FAN_MARK_INODE          0x00000000
+#define FAN_MARK_MOUNT          0x00000010
+'''
+FAN_MARK_FLAGS = CDefine(FAN_MARK_FLAGS_C)
+
+def fanotify_fdinfo(mark):
+	mflags = 0
+	if mark.flags & FSNOTIFY_MARK_FLAG_IGNORED_SURV_MODIFY:
+		mflags = mflags | FAN_MARK_FLAGS['FAN_MARK_IGNORED_SURV_MODIFY']
+	if mark.connector.type == enumerator_value("FSNOTIFY_OBJ_TYPE_INODE"):
+		inode = fsnotify_conn_inode(mark.connector)
+		print("inode: 0x{:016x}  ".format(inode), end='')
+		if not inode:
+			return
+		print("fanotify ino:{:x} sdev:{:x} mflags:{:x} mask:{:x} ignored_mask:{:x} ".format(
+			inode.i_ino, inode.i_sb.s_dev, mflags, mark.mask, mark.ignored_mask), end='')
+		show_mark_fhandle(inode)
+		print("")
+	elif mark.connector.type == enumerator_value("FSNOTIFY_OBJ_TYPE_VFSMOUNT"):
+		mnt = fsnotify_conn_mount(mark.connector)
+		print("fanotify mnt_id:{:x} mflags:{:x} mask:{:x} ignored_mask:{:x}".format(
+			mnt.mnt_id, mflags, mark.mask, mark.ignored_mask))
+	elif mark.connector.type == enumerator_value("FSNOTIFY_OBJ_TYPE_SB"):
+		sb = fsnotify_conn_sb(mark.connector)
+		print("fanotify sdev:{:x} mflags:{:x} mask:{:x} ignored_mask:{:x}".format(
+			sb.s_dev, mflags, mark.mask, mark.ignored_mask))
+
 def inotify_fdinfo(mark):
 	mark = readSU("struct fsnotify_mark", mark)
 	if not mark or not mark.connector:
@@ -77,12 +153,21 @@ def inotify_fdinfo(mark):
 		show_mark_fhandle(inode)
 		print("")
 
-
-def inotify_show_fdinfo(file):
+def notify_show_fdinfo(file, show):
 	group = readSU("struct fsnotify_group", file.private_data)
 	for mark in readSUListFromHead(group.marks_list, "g_list", "struct fsnotify_mark"):
 		print("mark: 0x{:016x}  ".format(mark), end='')
-		inotify_fdinfo(mark)
+		show(mark)
+
+def inotify_show_fdinfo(file):
+	notify_show_fdinfo(file, inotify_fdinfo)
+
+def fanotify_show_fdinfo(file):
+	group = readSU("struct fsnotify_group", file.private_data)
+	print("fanotify flags:%x event-flags:%x".format(group.fanotify_data.flags, group.fanotify_data.f_flags))
+
+	notify_show_fdinfo(file, fanotify_fdinfo)
+
 
 
 def real_mount(vfsmnt):
@@ -101,10 +186,16 @@ def display_fdinfo(addr):
 
 #	show_fd_locks?
 
+	if file.f_op and not file.f_op.show_fdinfo:
+		return
+
 	fops = addr2sym(file.f_op)
 	if fops == "inotify_fops":
 		print("inotify type")
 		inotify_show_fdinfo(file)
+	elif fops == "fanotify_fops":
+		print("fanotify type")
+		fanotify_show_fdinfo(file)
 	else:
 		print("unknown type")
 
